3a91cb1dd945dfca10145b70bfb4f411d5a0c25c
water <arc.cactus@yandex.ru>
2024-01-24 16:15:45 +0300

    Merge()

---
+++ LeetCode 21. Merge Two Sorted Lists/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+struct ListNode * Merge(ListNode* L, ListNode* R);
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+int main() {
+    ListNode L1(3);
+    ListNode L2(2,&L1);
+    ListNode L3(1, &L2);
+
+    ListNode R1(6);
+    ListNode R2(5, &R1);
+    ListNode R3(4, &R2);
+    ListNode * ANS = Merge(&L3, &R3);
+
+	return 0;
+}
+
+struct ListNode * Merge(ListNode * L, ListNode * R) {
+    ListNode* head, * temp;
+    head = new ListNode();
+    temp = head;
+
+
+    while (L && R) {
+        if (L->val < R->val) {
+            temp->next = L;
+            L = L->next;
+        }
+        else {
+            temp->next = R;
+            R = R->next;
+        }
+        temp = temp->next;
+    }
+    if (L) { temp->next = L; }
+    if (R) { temp->next = R; }
+    return head;
+}
+
+
+
